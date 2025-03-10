e5d447b42caa09f4ac416b313d50ca928788053e
radek <rajkonkret660@gmail.com>
2025-03-10 15:10:03 +0100

    [1 4] add typy_danych_kolekcje.py

---
+++ day_1/typy_danych_kolekcje.py
@@ -0,0 +1,74 @@
+# kolekcja
+# lista, krotka (tupla), zbiór (set), słownik
+
+# lista  - dowolna ilosc danych, dowolnego typu na raz
+# zachowuje kolejność przy dodawaniu elementów
+lista = []
+print(lista)  # []
+lista = list()
+print(lista)  # []
+print(type(lista))  # <class 'list'>
+
+lista.append("Radek")
+lista.append("Radek")
+lista.append("Radek")
+lista.append("Radek")
+lista.append("Radek")
+print(lista)  # ['Radek', 'Radek', 'Radek', 'Radek', 'Radek']
+
+lista2 = lista  # kopia referencji, adres w pamięci
+print(lista2)
+print(lista)
+# ['Radek', 'Radek', 'Radek', 'Radek', 'Radek']
+# ['Radek', 'Radek', 'Radek', 'Radek', 'Radek']
+lista_copy = lista.copy()  # kopia wszystkich elementów listy
+lista.clear()  # usunięcie wszystkich elementów z listy
+print(lista2)
+print(lista)
+print(lista_copy)
+# []
+# []
+# ['Radek', 'Radek', 'Radek', 'Radek', 'Radek']
+print(id(lista))
+print(id(lista2))
+print(id(lista_copy))
+# 2816861257664
+# 2816861257664
+# 2816858611968
+
+# krotka, tupla - niemutowalna kolekcja, tylko do odczytu
+# krotka pozwala lepiej zarzadzać pamięcią
+krotka = tuple(lista_copy)  # tuple() - rzutowanie na krotkę
+print(krotka)  # ('Radek', 'Radek', 'Radek', 'Radek', 'Radek')
+print(type(krotka))  # <class 'tuple'>
+
+krotka_liczby = 6, 7, 8, 9, 200
+print(krotka_liczby)  # (6, 7, 8, 9, 200)
+print(type(krotka_liczby))  # <class 'tuple'>
+
+krotka_jeden = ("Radek")
+print(type(krotka_jeden))  # <class 'str'>
+krotka_jeden = "Radek",
+print(type(krotka_jeden))  # <class 'tuple'>
+# PEP8 zaleca przy jednoelementowej krotce używać nawiasów
+krotka_jeden = ("Radek",)
+print(type(krotka_jeden))
+print(krotka_liczby.count(9))  # występuje 1 raz
+print(krotka_liczby.index(9))  # index numer 3 (pozycja 4)
+# krotka_liczby[0] = 7  # TypeError: 'tuple' object does not support item assignment
+# garbage collector - automatyczne usunięcie zbednych danych z pamięci
+del krotka_jeden  # usuniecie calej krotki
+# print(krotka_jeden)  # NameError: name 'krotka_jeden' is not defined
+
+# rozpakowanie krotki
+a, b = 1, 2
+print(f"{a=}, {b=}")
+tupla = 1, 2
+a, b = tupla
+print(f"{a=}, {b=}")
+tupla = 1, 2, 3
+# a, b = tupla # ValueError: too many values to unpack (expected 2)
+a, *b = tupla  # * pozostałe
+print(f"{a=}, {b=}") # a=1, b=[2, 3]
+
+
