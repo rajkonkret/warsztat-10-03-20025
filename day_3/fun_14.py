2a8282192b93547a9c0971445488922539340a9e
radek <rajkonkret660@gmail.com>
2025-03-12 09:42:11 +0100

    [3 2] update fun_14.py

--- day_3/fun_14.py
+++ day_3/fun_14.py
@@ -11,3 +11,24 @@ transactions = [
 ]
 
 
+def filter_transactions(transactions, transaction_type):
+    return list(filter(lambda x: x['type'] == transaction_type, transactions))
+
+
+def map_tranactions(transactions, currency):
+    return list(map(lambda x: x['amount'] if x['currency'] == currency else 0, transactions))
+
+
+def reduce_transactions(transactions):
+    return reduce(lambda x, y: x + y, transactions, 0)
+
+
+def process_transactions(transactions, transaction_type, currency):
+    filtered = filter_transactions(transactions, transaction_type)
+    mapped = map_tranactions(filtered, currency)
+    total = reduce_transactions(mapped)
+    return total
+
+
+result = process_transactions(transactions, "income", "EUR")
+print("Result:", result) # Result: 100
